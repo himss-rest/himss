72b64c1bdfbeabb1e5d73bd1e293dad98629a9ae
roywiner <18705179145@163.com>
2016-11-20 21:49:47 +0800

    Rejected commit: Default

---
+++ src/com/everycare/hello/Greeting.java
@@ -0,0 +1,23 @@
+package com.everycare.hello;
+
+/**
+ * Created by roy_luo on 2016/11/20.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
